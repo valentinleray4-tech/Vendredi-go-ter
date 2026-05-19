f2be4e4aff745c72f8d71faee9013baa8045c988
valentinleray4-tech <valentin.leray4@gmail.com>
2026-05-19 14:21:42 +0200

    Update Supabase URL for API access

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm'
 
 // 2. Configuration (Remplace avec TES propres identifiants Supabase)
-const SUPABASE_URL = 'https://vdetqssagjdlvpffmorp.supabase.co'
+const SUPABASE_URL = 'https://vdetqssagjdlvpffmorp.supabase.co/rest/v1/'
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZkZXRxc3NhZ2pkbHZwZmZtb3JwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkxODQyNDUsImV4cCI6MjA5NDc2MDI0NX0.kMmilrlCcQP8qLy76ClvwfsHY774iLxJjGOfT8tQsNg'
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
